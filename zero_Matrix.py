50cf76888bcef9e313706a403652d96eb4ae96f8
Rajadurai-CSE <rajadurai3491@gmail.com>
2024-03-03 22:58:38 +0530

    Added

--- zero_Matrix.py
+++ zero_Matrix.py
@@ -1,4 +1,12 @@
 def zero_matrix_fun(rows,cols):
+  """
+  The function creates a zero matrix of a specified number of rows and columns.
+  
+  :param rows: The `rows` parameter in the `zero_matrix_fun` function represents the number of rows in the matrix that you want to create. It is used to specify the size of the matrix along the vertical dimension
+  :param cols: The `cols` parameter in the `zero_matrix_fun` function represents the number of columns in the matrix that will be created. It is used to specify the width of the matrix
+  :return: A matrix filled with zeros of size rows x cols is being returned.
+  """
+  
   matrix = []
   for i in range(rows):
     for j in range(cols):
